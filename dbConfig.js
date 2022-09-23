7021d1e4436c80098068ccbb3f94832c5d16aa7b
unknown <vigenshvicky3122@gamil.com>
2022-09-23 15:21:01 +0530

    first commit

---
+++ dbConfig.js
@@ -0,0 +1,5 @@
+const Mongodb = require('mongodb');
+const MongoClient = Mongodb.MongoClient;
+const DbName ="Roooms";
+const DbURL = "mongodb+srv://wikky:Admin326@wikky.niqdc7c.mongodb.net/test"
+module.exports = {Mongodb,MongoClient,DbName,DbURL};
---
+++ routes/users.js
@@ -0,0 +1,221 @@
+var express = require('express');
+const {Mongodb,MongoClient,DbName,DbURL} = require('../dbConfig');
+const cors = require('cors');
+const {hashPassword,hashCompare} = require('../bin/auth');
+var router = express.Router();
+
+
+
+
+const client = new MongoClient(DbURL);
+
+router.use(cors({
+  origin: "http://localhost:3000"
+}))
+/* GET users listing. */
+router.get('/user', async(req, res)=> { 
+    await client.connect();
+  
+    try {
+      const db = await client.db(DbName);
+  let users = await db.collection('users').find().toArray();
+  res.json({ 
+      statusCode: 200,
+      users
+  })
+    } catch (error) {
+      res.json({ 
+          statusCode: 500,
+          message:"internal error occurred",
+          error: error,
+      });
+    }
+    finally { 
+       client.close();
+    }
+  });
+
+router.get('/user/:id', async(req, res)=> { 
+    await client.connect();
+    try {
+      const db = await client.db(DbName);
+  let users = await db.collection('users').find({_id:Mongodb.ObjectId(req.params.id)}).toArray();
+  
+  res.json({ 
+      statusCode: 200,
+      users
+  })
+    } catch (error) {
+      res.json({ 
+          statusCode: 500,
+          message:"internal error occurred",
+          error: error,
+      });
+    }
+    finally { 
+       client.close();
+    }
+  });
+
+  router.put('/reset/:id', async(req, res)=> {
+  await client.connect();
+
+  try 
+{
+    const  db = await client.db(DbName);
+    let data = await db.collection('users').find({_id:Mongodb.ObjectId(req.params.id)}).toArray();
+    
+  if(data[0].email===req.body.email)
+  {
+    if(req.body.password)
+    {
+      req.body.password = await hashPassword(req.body.password);
+
+      let users =  await db.collection('users').updateOne({_id:Mongodb.ObjectId(req.params.id)},{$set:req.body});
+        
+        res.json(
+        {
+          statusCode: 200,
+          message: "Do you have change the Details?",
+          users
+        })
+    }
+    else 
+    { 
+      res.json(
+      {
+      statusCode: 400, 
+      message: "credentials does not match (: "
+      }) 
+    }
+
+  }
+  else{
+    res.json(
+      {
+      statusCode: 401, 
+      message: "invalid details email does not change"
+      }) 
+  }
+} 
+catch (error) {
+    res.json({ 
+        statusCode: 500,
+        message:"internal error occurred",
+        error: error,
+    });
+  }
+  finally { 
+     client.close();
+  }
+});
+
+router.post("/sign-up", async (req, res) => {
+    await client.connect()
+    try {
+        const db = await client.db(DbName);
+        let user = await db.collection('users').find({email:req.body.email}).toArray();
+        if (user.length === 0) {
+            
+            req.body.password = await hashPassword(req.body.password);
+            let user = await db.collection('users').insertOne(req.body);
+            res.json({
+                statusCode: 200,
+                message : "Do you want create a new user?",
+                user
+            })
+        } else {
+            res.json({
+                statusCode: 401,
+                message : "this user was already existing here Please Login!"
+            })
+        }
+    } 
+    catch (error) {
+        res.json({
+            statusCode: 500,
+            message : "internal server error"
+        })
+    }
+    finally 
+    {   
+client.close()
+    }
+
+})
+
+
+
+router.post("/login", async (req, res) => {
+    await client.connect()
+    try {
+
+      const db = await client.db(DbName);
+
+        let user = await db.collection('users').find({email:req.body.email}).toArray();
+        
+        if (user.length===1){
+          
+              if(req.body.password===req.body.confirm_password){
+
+                let hashResult = await hashCompare(req.body.password,user[0].password);
+
+                    if (hashResult) 
+                      {
+                            res.json({
+                            statusCode: 200,
+                            message : "if you want to login!"});
+                      } 
+                    else 
+                      {
+                        res.json({
+                          statusCode: 401,
+                          message : "incorrect Details ^_^"});
+                      }}
+              else 
+                  {
+                    res.json({
+                      statusCode: 402,
+                      message : "details does not match"});
+                  } }
+               
+        else {
+          res.json({
+            statusCode: 404,
+            message :"user does not exist Please DO Register -->" });
+            }
+        
+      }  
+    
+catch
+{
+    res.json({
+    statusCode: 500,
+    message : "internal server error"})
+}
+finally 
+{   
+client.close()
+}
+
+})
+
+router.delete('/delete-user/:id', async (req, res)=> {
+  await client.connect()
+  try {
+    const  db = await client.db(DbName);
+let data = await db.collection('users').deleteOne({_id:Mongodb.ObjectId(req.params.id)});
+res.json({
+    statusCode: 200,
+    message: "Are you sure delete this user?",
+})
+}catch (error) {
+  res.json({
+    statusCode: 500,
+    message : "internal server error"})
+}finally {  
+client.close()
+}
+})
+
+module.exports = router;
