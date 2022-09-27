fd8ff00f2fe4355c3c00e89791385c9c17562710
vigneshvicky3122 <105697437+vigneshvicky3122@users.noreply.github.com>
2022-09-27 17:55:44 +0530

    Update ViewStudent.js

--- src/components/ViewStudent.js
+++ src/components/ViewStudent.js
@@ -34,7 +34,7 @@ function ViewStudent() {
   };
 
   let handleDelete = async (i) => {
-    let navigate = useNavigate();
+   
     try {
       let rest = await axios.delete(`${`${url}/delete-user/${i}`}`, {
         headers: { authorization: window.localStorage.getItem("app-token") },
@@ -42,7 +42,7 @@ function ViewStudent() {
 
       if (rest.data.statusCode === 200) {
         window.alert(rest.data.message);
-        navigate("/user")
+        
       }
       if (rest.data.statusCode === 404) {
         window.alert(rest.data.message);
