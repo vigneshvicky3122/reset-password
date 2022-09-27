43eb5edc4b4b528bbf8b3a507845fdc8c86c2337
vigneshvicky3122 <105697437+vigneshvicky3122@users.noreply.github.com>
2022-09-27 11:59:50 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -25,7 +25,7 @@ let authentication = async (req, res, next) => {
 };
 router.use(
   cors({
-    origin: "http://63328db2dc8d115104109a4b--helloteches.netlify.app",
+    origin: "https://63328db2dc8d115104109a4b--helloteches.netlify.app",
   })
 );
 
